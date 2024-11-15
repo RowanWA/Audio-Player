d273c4bd52d58a1a42b162f18a46a701ce85b524
RowanTheAccussed <126895389+RowanTheAccussed@users.noreply.github.com>
2024-11-15 15:18:42 +0000

    final

--- draggable.js
+++ draggable.js
@@ -1,46 +1,24 @@
-const audioDrag = document.getElementById("flex-container");
-const dropZone = document.getElementById("body-container");
-const volSliderDragExemption = document.getElementById("volume-slider");
-const proSliderDragExemption = document.getElementById("progress-slider");
-
-let draggable = false;
-
-function volDragStop(event) {
-    draggable = false;
-}
-function proDragStop(event) {
-    draggable = false;
-}
-
+const audioDrag = document.getElementById ("flex-container");
+const dropZone = document.getElementById ("body-container");
 
 function onDrop(event) {
-    if (draggable = true) {
-        audioDrag.style.left = event.clientX - offsetX + "px";
-        audioDrag.style.top = event.clientY - offsetY + "px";
-    }
+    audioDrag.style.left = event.clientX - offsetX + "px";
+    audioDrag.style.top = event.clientY - offsetY + "px";
 }
 
 function onDragOver(event) {
-    if (draggable = true) {
-        event.preventDefault();
-    }
+    event.preventDefault();
 }
 
 let offsetX = 0;
 let offsetY = 0;
 
 function onDragStart(event) {
-    if (draggable = true) {
-        const style = window.getComputedStyle(audioDrag, null);
-        offsetX = event.clientX - parseInt(style.left);
-        offsetY = event.clientY - parseInt(style.top);
-    }
+    const style = window.getComputedStyle(audioDrag, null);
+    offsetX = event.clientX - parseInt(style.left);
+    offsetY = event.clientY - parseInt(style.top);
 }
-
-
 
 dropZone.ondrop = onDrop;
 dropZone.ondragover = onDragOver;
 audioDrag.ondragstart = onDragStart;
-volSliderDragExemption.onmouseover = volDragStop;
-proSliderDragExemption.onmouseover = proDragStop;
